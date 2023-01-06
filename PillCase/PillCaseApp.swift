42c03bbc04ecae4747b0374e976d5d786f298fc3
voidgxd <118388964+voidgxd@users.noreply.github.com>
2023-01-06 23:28:56 +0300

    Initial Commit

---
+++ PillCase/PillCaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  PillCaseApp.swift
+//  PillCase
+//
+//  Created by Максим Мосалёв on 06.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct PillCaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
